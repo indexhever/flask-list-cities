29b67b892b16ed68a89b9a2333696fde52c415d5
heverton <heverton.sarah@gmail.com>
2017-04-11 21:23:07 -0300

    add migrations and models

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-set DATABASE_URL="sqlite:////D:/Arre Games/Python/flaskProject/projtest/testDb.db"
+set DATABASE_URL=sqlite:///testDb.db
 
 set APP_SETTINGS="config.DevelopmentConfig"
+
+C:\Users\Heverton\Downloads\sqlite-tools-win32-x86-3180000/sqlite3 testDb.db 
--- models.py
+++ models.py
@@ -1,12 +1,11 @@
 from app import db
 
 class Cities(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'cities'
     id = db.Column(db.Integer, primary_key=True)
     cityname = db.Column(db.String(50), unique=True)
 
-    def __init__(self, name=None, cityname=None):
-        self.name = name
+    def __init__(self, cityname=None):
         self.cityname = cityname
 
     def __repr__(self):
